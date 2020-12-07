91eeeefb36b0e1cf3bd782bc1b696117996e320d
Alexandre Feio <alefeio@gmail.com>
2020-12-07 03:05:25 -0300

    novo push7

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,10 @@ require('dotenv/config');
 
 module.exports = {
   dialect: 'postgres',
-  host: '142.93.181.196',
-  username: postgres,
-  password: docker,
-  database: dhages,
+  host: 'localhost',
+  username: 'postgres',
+  password: 'docker',
+  database: 'dhages',
   define: {
     timestamps: true,
     underscored: true,
