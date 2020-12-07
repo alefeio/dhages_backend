23e5eb1fed8b5d270df9ab9c87dd610c74306d73
Alexandre Feio <alefeio@gmail.com>
2020-12-07 12:26:10 -0300

    alteracoes

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ require('dotenv/config');
 
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
+  host: '142.93.181.196',
   username: 'postgres',
   password: 'docker',
   database: 'dhages',
