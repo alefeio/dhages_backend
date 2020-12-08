b471c7d75d889f033a81f05af3e59aa6af799e6d
Alexandre Feio <alefeio@gmail.com>
2020-12-08 13:24:07 -0300

    enviomigrations2

--- src/config/database.js
+++ src/config/database.js
@@ -6,7 +6,6 @@ module.exports = {
   username: process.env.BD_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: 3333,
   define: {
     timestamps: true,
     underscored: true,
